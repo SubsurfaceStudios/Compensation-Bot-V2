bef5dc38bf3b7ae8b773fb0c07617dc3223ae43e
Raven <82341739+bubby932@users.noreply.github.com>
2022-05-09 17:39:53 -0400

    added analytics and finished help command

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,15 @@ module.exports = {
         const embed = new MessageEmbed()
             .setTitle("Compensation VR Bot Help")
             .addField("/help", "This command is used to list all commands for the Compensation Bot. You just used it!")
-            .addField("/test", "This command tests out a variety of features of the Compensation Bot, primarily for the bot developers.");
+            .addField("/test", "This command tests out a variety of features of the Compensation Bot, primarily for the bot developers.")
+            .addField("/item", "Fetches information about a CVR in-game item from the servers. Please note this uses the item ID not the item name.")
+            .addField("/user", "Returns information about a user from their username.")
+            .addField("/room-search", "Searches for a list of rooms by name. Maximum number of results is 5.")
+            .addField("/staff-applications", "Posts information about becoming staff.")
+            .addField("/mod-applications", "Posts information about becoming a CVR moderator.")
+            .addField("/room", "Returns information about a room from it's ID.")
+            .addField("/website", "Returns a link to the Compensation VR website. Use `status:true` to display whether the site is online or not.")
+            .addField("/analytics", "Returns analytical information about the Compensation VR servers.");
         interaction.reply({embeds: [embed], content: "Done!"});
     }
 }
